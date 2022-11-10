b6774af6532fbbc5434ae61ac9f2bbaf33cd37f5
MarkBenedictMuyot <markmuyot@hotmail.com>
2022-11-10 02:49:36 -0500

    added course controller and defined entity relationships in course entity

--- src/main/java/ca/vanier/vanierapi/Controller/CourseController.java
+++ src/main/java/ca/vanier/vanierapi/Controller/CourseController.java
@@ -1,5 +1,41 @@
 package ca.vanier.vanierapi.Controller;
 
+import ca.vanier.vanierapi.*;
+import ca.vanier.vanierapi.Entity.Course;
+import ca.vanier.vanierapi.Service.CourseService;
+
+import java.util.List;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class CourseController {
-    
+    @Autowired
+    private CourseService courseService;
+
+    // Save operation
+    @PostMapping("/course")
+    public Course saveCourse(@Valid @RequestBody Course course) {
+        return courseService.saveCourse(course);
+    }
+
+    // Read operation
+    @GetMapping("/course")
+    public List<Course> fetchCourseList() {
+        return courseService.listCourse();
+    }
+
+    // Update operation
+    @PutMapping("/course/{id}")
+    public Course updateCourse(@RequestBody Course course, @PathVariable("id") Integer courseId) {
+        return courseService.updateCourse(course,courseId);
+    }
+
+    // Delete operation
+    @DeleteMapping("/course/{id}")
+    public String deleteCourseById(@PathVariable("id") Integer courseId) {
+        courseService.deleteCourseID(courseId);
+        return "Deleted Successfully";
+    }
 }
--- src/main/java/ca/vanier/vanierapi/Entity/Course.java
+++ src/main/java/ca/vanier/vanierapi/Entity/Course.java
@@ -2,11 +2,14 @@ package ca.vanier.vanierapi.Entity;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -23,8 +26,11 @@ public class Course {
     private String courseName;
     @Column
     @OneToOne
+    @JoinColumn(name = "teacher_ID", referencedColumnName = "id")
     Teacher teacher;
     @Column
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "students_ID", referencedColumnName = "id")
     List<Student> students = new ArrayList<>();
 
     public Course(int id, String courseName, Teacher teacher, List<Student> students) {
